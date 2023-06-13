6c10a5a42cc9534776a6db41a534472c62d03585
Tai <tantaivo74@gmail.com>
2023-06-13 10:33:43 +0700

    update

--- connection.js
+++ connection.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
 import {dbConfig} from './src/config/db.config';
 import mysql2 from 'mysql2';
-const sequelize = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
-  host: dbConfig.host,
+const sequelize = new Sequelize('node-db-test_base', '316579_admin', '@Hieupm2k03', {
+  host: 'mysql-node-db-test.alwaysdata.net',
   port: 3306, // The port of the relational database, MySQL is 3306 by default
   dialect: 'mysql',
   // dialectModule: mysql2, // To use mysql2 module instead of mysql module
@@ -13,7 +13,8 @@ const sequelize = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.passw
   //   acquire: 1000000,
   //   idle: 100000,
   //   evict: 2000,
-  // }, // 5 seconds to release a connection back to the pool after it has been evicted in idle state (unused in the pool)
+  // },
+  // 5 seconds to release a connection back to the pool after it has been evicted in idle state (unused in the pool)
   dialectOptions: {
     decimalNumbers: true, // Prevents sequelize from converting decimals to strings when fetching data from database 
   },
