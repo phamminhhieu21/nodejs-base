987fc8135f05b0f1050b5f05ce768acd19971f00
Tai <tantaivo74@gmail.com>
2023-06-13 09:12:43 +0700

    update

--- connection.js
+++ connection.js
@@ -5,15 +5,15 @@ const sequelize = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.passw
   host: dbConfig.host,
   port: 3306, // The port of the relational database, MySQL is 3306 by default
   dialect: 'mysql',
-  dialectModule: mysql2, // To use mysql2 module instead of mysql module 
+  // dialectModule: mysql2, // To use mysql2 module instead of mysql module
   logging: false,
-  pool: {
-    max: 100,
-    min: 0,
-    acquire: 1000000,
-    idle: 100000,
-    evict: 2000,
-  }, // 5 seconds to release a connection back to the pool after it has been evicted in idle state (unused in the pool) 
+  // pool: {
+  //   max: 100,
+  //   min: 0,
+  //   acquire: 1000000,
+  //   idle: 100000,
+  //   evict: 2000,
+  // }, // 5 seconds to release a connection back to the pool after it has been evicted in idle state (unused in the pool)
   dialectOptions: {
     decimalNumbers: true, // Prevents sequelize from converting decimals to strings when fetching data from database 
   },
